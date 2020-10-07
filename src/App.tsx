7011d2e54be08cff4e32f85d177dda63762fc839
Andronikus <tiagoafroque@gmail.com>
2020-10-07 23:32:02 +0100

    add router to project

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,20 @@
 import React from 'react'
+import { BrowserRouter, Route, Switch } from 'react-router-dom'
+
 import './App.css'
 
 function App() {
-  return <h1>Helloo</h1>
+   return (
+      <>
+         <BrowserRouter>
+            <Switch>
+               <Route path="/login" render={() => <h1>Login</h1>} />
+               <Route path="/signIn" render={() => <h1>Sign In</h1>} />
+               <Route exact path="/" render={() => <h1>Pomodoro page</h1>} />
+            </Switch>
+         </BrowserRouter>
+      </>
+   )
 }
 
 export default App
