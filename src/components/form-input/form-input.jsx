9e9a54ed0fb1a3a825655b90698a907d09ea2909
Amdronikus <tiagoafroque@gmail.com>
2020-12-29 21:53:26 +0000

    add form input component

--- src/components/form-input/form-input.jsx
+++ src/components/form-input/form-input.jsx
@@ -1,20 +1,25 @@
-import React from 'react';
+import React from 'react'
 
-import './form-input.scss'
+import './form-input.css'
+import icons from './symbol-defs.svg'
 
 const FormInput = () => {
-    return (
-        <div className="form-input">
-            <input type="text" className="form-input__input" placeholder='your-email@mail.com' />
-            <div className="form-input__img-container">
-                <div className="form-input__img">
-                    <svg className="form-input__icon">
-                        <use xlinkHref="/symbol-defs.svg#icon-person"></use>
-                    </svg>
-                </div>
+   return (
+      <>
+         <div className="form-input">
+            <label className="form-input__label">Email</label>
+            <div className="form-input__input-container">
+               <input type="text" className="form-input__input" placeholder="your-email@mail.com" />
+               <div className="form-input__icon-container">
+                  <svg className="form-input__icon">
+                     <use xlinkHref={`${icons}#icon-person`}></use>
+                  </svg>
+                  <div className="form-input__icon-line"></div>
+               </div>
             </div>
-        </div >
-    );
-};
+         </div>
+      </>
+   )
+}
 
-export default FormInput;
+export default FormInput
